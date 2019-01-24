99ec21e0c6e35ba42bb082bfdf62126c6eec0f76
DuduValadares <39996223+DuduValadares@users.noreply.github.com>
2019-01-23 23:52:26 -0200

    array de um numero pra 0

---
+++ fase06/ex01/ateZero.js
@@ -0,0 +1,10 @@
+function ateZero(num) {
+    var x = [];
+    if (num > 0)
+        for (i = 0; i <= num; i++)
+        x.push(i);
+    if (num < 0)
+        for (i = num; i <= 0; i++)
+        x.push(i)
+    console.log(x);
+    }
